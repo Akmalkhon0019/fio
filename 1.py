83a119c5090984eda33e9a3ad5d9746549738293
Akmalxon <akmal93@gmail.com>
2024-07-14 22:38:49 +0500

    why

--- 1.py
+++ 1.py
@@ -1,3 +1,9 @@
 name = [('Qutbidinxon',3),('Madaminov',1),('ogli',4),('Akmalkhon',2)]
 name.sort(key=lambda i: i[1])
 print(name)
+f = name[0][0]
+i = name[1][0]
+o = name[2][0]
+z = name[3][0]
+fio = f+ ' '+ i +' '+ o + ' '+ z
+print(fio)
